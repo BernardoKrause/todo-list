cc997c4d5c35e721ff00e8b08ba76e96468e66f5
Bernardo Krause Rodrigues <bernardokrause@hotmail.com>
2024-10-19 18:20:00 -0300

    Interfaces de usuário estilizadas

--- app/components/header_component.rb
+++ app/components/header_component.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class HeaderComponent < ViewComponent::Base
+  def initialize(current_user: :current_user)
+    @current_user = current_user
+  end
+
+  def user_signed_in?
+    helpers.user_signed_in?
+  end
+
+  def current_user
+    helpers.current_user
+  end
 end
