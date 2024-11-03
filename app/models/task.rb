4b47ac8f710dabe8232d78ad81d589e9596ffe97
Bernardo Krause Rodrigues <bernardokrause@hotmail.com>
2024-11-03 19:26:40 -0300

    deleção de código lixo no tasks controller e adição de order by prazo na lista de tarefas

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :list, optional: true
   enum priority: { baixa: 0, media: 1, alta: 2 }
-  scope :by_priority, -> { order(concluido: :asc, priority: :desc) }
+  scope :by_priority, -> { order(concluido: :asc, priority: :desc, deadline: :asc) }
   validates :description, presence: true
 end
